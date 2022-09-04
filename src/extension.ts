eafcd9250d0a2656b0f68583eda7d1b35455c18b
DevMoxi <moxiout@gmail.com>
2022-09-04 13:31:54 +0800

    clear repo

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,18 @@
 /*
  * @Author: moxi moxiout@gmail.com
  * @Date: 2022-08-24 10:09:36
- * @LastEditTime: 2022-08-26 17:53:20
+ * @LastEditTime: 2022-09-04 13:20:29
  */
 import * as vscode from "vscode";
 import { WorkspaceHandler } from "./handler/WorkSpaceHandler";
 
 async function activate(context: vscode.ExtensionContext) {
-  // init handler
-  new WorkspaceHandler(context)
-    .init(context)
-    .snippetCompletion()
-    .onCreateFile();
-  // init debugger
+	// init handler
+	new WorkspaceHandler(context)
+		.init(context)
+		.snippetCompletion()
+		.onCreateFile();
+	// init debugger
 }
 
 exports.deactivate = function () {};
